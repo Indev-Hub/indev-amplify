0130c84481778198577412dd718a943e88c0f63a
Woo Jin Kim <300hHZ@gmail.com>
2021-07-08 14:57:30 -0700

    feature: attempt to create ShowcaseLibrary component

--- src/components/home/HomeHero.js
+++ src/components/home/HomeHero.js
@@ -9,7 +9,7 @@ import ChannelBrowse from '../../pages/channel/ChannelBrowse';
 import ChannelSlider from '../channel-slider/ChannelSlider';
 import FeaturedSlider from '../featured-slider/FeaturedSlider';
 import ShowcaseAdd from '../video/ShowcaseAdd';
-import ShowcaseLibrary from '../video/ShowcaseLibrary';
+import Showcase from '../../pages/dashboard/Showcase';
 // import HomeOverview from './HomeOverview';
 // import ChannelSlider1 from './ChannelSlider';
 
@@ -150,7 +150,7 @@ const HomeHero = (props) => (
       {/* <VideoUpdate /> */}
       {/* <UploadVideo /> */}
       {/* <ShowcaseAdd /> */}
-      <ShowcaseLibrary />
+      <Showcase />
       <Typography variant="h4" style={{ marginTop: '0px' }}>Game Development</Typography>
       <ChannelSlider genre={game} sliderHeight="500px" />
       <Typography variant="h4" style={{ marginTop: '40px' }}>Software Development</Typography>
--- src/components/video/ShowcaseLibrary.js
+++ src/components/video/ShowcaseLibrary.js
@@ -1,112 +1,79 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react'
 import {
-	Box,
-	TextField,
-	IconButton,
-	Paper,
-	Typography,
-	Button
+	Box
 	// Typography
 } from '@material-ui/core'
-import { API, graphqlOperation, Storage } from 'aws-amplify'
-import { getShowcase } from '../../graphql/queries'
-import { updateShowcase } from '../../graphql/mutations';
-import { Grid } from '@material-ui/core'
-import { Publish } from '@material-ui/icons';
+import PropTypes from "prop-types";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 
-
-const ShowcaseLibrary = () => {
-	const [showcaseData, setShowcaseData] = useState({});
-	const [showcaseID, setShowcaseID] = useState(7868357)
-	const [videos, setVideos] = useState([]);
-	const [order, setOrder] = useState([]);
-
-	useEffect(() => {
-		fetchVideos();
-	}, [])
-
-	const fetchVideos = async () => {
-		try {
-			const videoData = await API.graphql(graphqlOperation(getShowcase, { id: showcaseID}));
-			const showcaseVideoData = videoData.data.getShowcase.videos;
-			const videoList = JSON.parse(showcaseVideoData);
-
-			console.log('videoData: ', videoData);
-			console.log('showcaseVideoData: ', showcaseVideoData);
-			console.log('videoList: ', videoList);
-			setVideos(videoList);
-			setShowcaseData(videoData.data.getShowcase);
-		} catch (error) {
-			console.log('error on fetching videos', error);
-		}
-	}
-
-	const saveShowcaseChanges = async () => {
-		const switchedVideos = [videos[1],videos[0]];
-		const newVideos =  switchedVideos.concat(videos.slice(2));
-		const updatedShowcase = {
-			id: showcaseData.id,
-			title: showcaseData.title,
-			manager: showcaseData.manager,
-			managerID: showcaseData.managerID,
-			videos: JSON.stringify(newVideos)
-		 };
-		console.log('showcaseData1:', showcaseData);
-		console.log('showcaseData videos1:', videos);
-		console.log('updatedShowcase1:', updatedShowcase);
-		await API.graphql(graphqlOperation(updateShowcase, { input: updatedShowcase}));
-		setShowcaseData(updatedShowcase)
-		setVideos(JSON.parse(updatedShowcase.videos));
-		console.log('showcaseData2', showcaseData);
-		console.log('showcaseData videos2', videos);
-	
-	}
-
+const ShowcaseLibrary = (props) => {
+	console.log('props ', props);
+	const { /*data, videos, */classes, onDragEnd, onDragUpdate, getListStyle, getItemStyle, placeholderProps} = props;
+	const { videos } = props.data.data.getShowcase;
+	console.log('videos ', videos);
+	const vidArray= JSON.parse(videos);
+	console.log('vidArray ', vidArray);
+	// const showcase = {...data.getShowcase,
+	// 										videos:vidArray
+	//									 }
 	return (
-		<Box
-			// width="100%"
-			alignItems="center"
-			justify="center"
-			margin="auto"
-		>
-			{/* <TextField
-				label="Showcase ID"
-				onChange={e => setShowcaseID(e.target.value)}
-			/>
-			<Button
-				onClick={() => {fetchVideos(showcaseID)}}
-			> 
-				Load Data
-			</Button> */}
-
-			<IconButton onClick={saveShowcaseChanges}>
-				<Publish 
-					color="primary"/>
-			</IconButton>
-
-			{videos.map((showcase, idx) => {
-				return <Paper variant="outlined" sx={{py: 2, px: 5, m: 1}} key={`showcase_${idx}`}>
-					<Grid
-						container
-						display="flex"
-						className="videoCard"
-						direction="column"
-						alignItems="center"
-						justify="center"
+		<DragDropContext onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
+			<Droppable droppableId="droppable">
+				{(provided, snapshot) => (
+					<Box
+						{...provided.droppableProps}
+						ref={provided.innerRef}
+						style={getListStyle(snapshot.isDraggingOver)}
 					>
-						<Grid item xs={6}>
-							<Typography className="videoTitle">{showcase.name}</Typography>
-						</Grid>
-						<Grid item>
-							<Typography className="videoDescription">{showcase.uri}</Typography>
-						</Grid>
-					</Grid>
-				</Paper>
-			})}
-		</Box>
+						{vidArray.map((item, index) => (
+							
+							<Draggable key={item.uri} draggableId={item.uri} index={index}>
+								{(provided, snapshot) => (
+									<Box
+										ref={provided.innerRef}
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										style={getItemStyle(
+											snapshot.isDragging,
+											provided.draggableProps.style
+										)}
+									>
+										{item.name}
+										{/* <img className={classes.thumbnail} src={data.getShowcase.pictures.size[index].link} /> */}
+									</Box>
+								)}
+							</Draggable>
+						))}
+
+						{provided.placeholder}
+						 {/* <CustomPlaceholder snapshot={snapshot} />  */}
+						<Box style={{
+							position: "absolute",
+							top: placeholderProps.clientY,
+							left: placeholderProps.clientX,
+							height: placeholderProps.clientHeight,
+							background: "tomato",
+							width: placeholderProps.clientWidth
+						}} />
+					</Box> 
+				)}
+			</Droppable>
+		</DragDropContext>
 	)
+}
+
+ShowcaseLibrary.propTypes = {
+	// data: PropTypes.object,
+	videos: PropTypes.array,
+	classes: PropTypes.func,
+	onDragEnd: PropTypes.func,
+	onDragUpdate: PropTypes.func,
+	getListStyle: PropTypes.func,
+	getItemStyle: PropTypes.func,
+	fetchVideos: PropTypes.func,
+	placeholderProps: PropTypes.object
 }
 
 export default ShowcaseLibrary;

b7e3990d104f722c09d31160e0c07525f13a4b70
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-07-18 11:31:28 -0700

    refactor server to use .env instead of hardcode, resolve issue with origin url

--- stripe-server/index.js
+++ stripe-server/index.js
@@ -4,12 +4,12 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const { resolve } = require("path");
 const session = require("express-session");
-const stripe = require("stripe")('sk_test_51GTdOLEXAhfvjJuXkywMIvsosTmdaTIgGw2WBV6AJEPlcnlBSYbrrAFCXS09M5ORmV90xz3he4Zblk9R94zCwhT900tRGBIExP');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 4200;
 
-app.use(express.static('./../src'));
+app.use(express.static(process.env.STATIC_DIR));
 app.use(session({
   secret: "Set this to a random string that is kept secure",
   resave: false,
@@ -26,12 +26,12 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  // const path = resolve(process.env.STATIC_DIR + "/index.html");
-  const path = resolve('./../src/index.html')
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  // const path = resolve('./../src/index.html')
   res.sendFile(path);
 });
 
-const endpointSecret = 'whsec_67hJ1pyr5ZG7mRR0fB85YjMKsShpjnHB';
+const endpointSecret = process.env.WH_SECRET_KEY;
 
 app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
   const sig = request.headers['stripe-signature'];
@@ -59,7 +59,9 @@ app.post("/onboard-user", async (req, res) => {
     const account = await stripe.accounts.create({type: "express"});
     req.session.accountID = account.id;
 
-    const origin = `${req.secure ? "https://" : "http://"}${req.headers.origin}`;
+    // const origin = `${req.secure ? "https://" : "http://"}${req.headers.origin}`;
+    const origin = `${req.headers.origin}`;
+    console.log(origin)
     const accountLinkURL = await generateAccountLink(account.id, origin);
     res.send({
       url: accountLinkURL
@@ -78,7 +80,8 @@ app.get("/onboard-user/refresh", async (req, res) => {
   }
   try {
     const {accountID} = req.session;
-    const origin = `${req.secure ? "https://" : "http://"}${req.headers.host}`;
+    // const origin = `${req.secure ? "https://" : "http://"}${req.headers.host}`;
+    const origin = `${req.headers.origin}`;
     
     const accountLinkURL = await generateAccountLink(accountID, origin)
     res.redirect(accountLinkURL);
@@ -98,10 +101,10 @@ const generateAccountLink = (accountID, origin) => {
   return stripe.accountLinks.create({
     type: "account_onboarding",
     account: accountID,
-    // refresh_url: `${origin}/onboard-user/refresh`,
-    // return_url: `${origin}/success.html`,
-    refresh_url: `http://localhost:4200/onboard-user/refresh`,
-    return_url: `http://localhost:4200/success.html`,
+    refresh_url: `${origin}/onboard-user/refresh`,
+    return_url: `${origin}/success.html`
+    // refresh_url: `http://localhost:4200/onboard-user/refresh`,
+    // return_url: `http://localhost:4200/success.html`,
   }).then((link) => link.url);
 }
 
